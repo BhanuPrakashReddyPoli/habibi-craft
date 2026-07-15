31c9777cad66f4985ec4552237cfc8823570f73d
Bhanu Prakash Reddy <bhanuprakashreddypoli@gmail.com>
2026-07-15 16:10:57 +0530

    corrected logo name and path, solved vercel deployment error

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -52,7 +52,7 @@ const Twitter = (props: React.SVGProps<SVGSVGElement>) => (
     <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
   </svg>
 );
-import logo from "@/assets/logo.png";
+import logo from "@/assets/Logo.png";
 import hero1 from "@/assets/hero-1.jpg";
 import hero2 from "@/assets/hero-2.jpg";
 import hero3 from "@/assets/hero-3.jpg";
